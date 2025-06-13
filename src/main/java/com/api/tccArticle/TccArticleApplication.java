31503327f33c7da7f65cc81e82a4f42bac9974a3
adriano_barbosa <adrianobarbosa3001@gmail.com>
2025-06-13 17:24:29 -0300

    feat:Iniciando projeto

---
+++ src/main/java/com/api/tccArticle/TccArticleApplication.java
@@ -0,0 +1,13 @@
+package com.api.tccArticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccArticleApplication.class, args);
+	}
+
+}
