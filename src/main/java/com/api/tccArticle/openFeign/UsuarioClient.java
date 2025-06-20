8cfdb3d2fdb69e0c68ada240bb76a2cfbc568f7a
Douglas Porto <portogdouglas@gmail.com>
2025-06-20 11:06:37 -0300

    feat: Configurando conexão com MongoDB e atualizando dependências do projeto

--- src/main/java/com/api/tccArticle/openFeign/UsuarioClient.java
+++ src/main/java/com/api/tccArticle/openFeign/UsuarioClient.java
@@ -1,13 +1,13 @@
 package com.api.tccArticle.openFeign;
 
 import com.api.tccArticle.domain.dto.UsuarioDTO;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "usuario-service", url = "http://localhost:8080")
 public interface UsuarioClient {
 
-    @GetMapping("/usuarios/{id}")
+    @GetMapping("/users/{id}")
     UsuarioDTO buscarPorId(@PathVariable String id);
 }
