72f87051a880bf047447894d6fbb9f5afe47dab0
Douglas Porto <portogdouglas@gmail.com>
2025-07-01 16:57:53 -0300

    feat: Remove campo de autor do ArticleDTO e melhora o UsuarioDTO com anotações de propriedades JSON

--- src/main/java/com/api/tccArticle/domain/dto/ArticleDTO.java
+++ src/main/java/com/api/tccArticle/domain/dto/ArticleDTO.java
@@ -1,5 +1,5 @@
 package com.api.tccArticle.domain.dto;
 import jakarta.validation.constraints.NotBlank;
 
-public record ArticleDTO(@NotBlank String title, @NotBlank String content, @NotBlank String cdAuthor) {
+public record ArticleDTO(@NotBlank String title, @NotBlank String content) {
 }
--- src/main/java/com/api/tccArticle/domain/dto/UsuarioDTO.java
+++ src/main/java/com/api/tccArticle/domain/dto/UsuarioDTO.java
@@ -1,5 +1,8 @@
 package com.api.tccArticle.domain.dto;
 
-public record UsuarioDTO(String cdUsuario,
-                         String nmUsuario) {
-}
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record UsuarioDTO(
+        @JsonProperty("id") String cdUsuario,
+        @JsonProperty("name") String nmUsuario
+) {}
